853e0c424b54f2738734d94ee3074b0f48ba4a31
Kosmonautt <b.contreras.2@ug.uchile.cl>
2024-12-04 23:29:46 -0300

    fix: update initial position and velocity constants in projectile script

--- kine-vr/Scripts/projectile.gd
+++ kine-vr/Scripts/projectile.gd
@@ -1,11 +1,12 @@
 extends RigidBody3D
 
-const speed: int = 5
+const initial_position: Vector3 = Vector3(-4, 1, 0)
+const intiial_velocity: Vector3 = Vector3(5, 5, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector3(-4, 1, 0)
-	linear_velocity = Vector3(speed, speed, 0)
+	position = initial_position
+	linear_velocity = intiial_velocity
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +15,5 @@ func _process(_delta):
 
 
 func _on_parabola_body_exited(_body):
-	print("exited")
+	position = initial_position
+	linear_velocity = intiial_velocity
