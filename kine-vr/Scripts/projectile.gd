e15ab3412a6079bb4cea585237d6bd89b2eb85e0
Kosmonautt <b.contreras.2@ug.uchile.cl>
2024-12-04 23:07:58 -0300

    feat: add Projectile node and script to parabola scene

---
+++ kine-vr/Scripts/projectile.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+const speed: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	linear_velocity = Vector3(speed, speed, 0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
