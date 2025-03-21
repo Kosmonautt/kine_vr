64d45ce2b998a9d0ccfc79bc902ac72deb6df60b
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-21 19:50:48 -0300

    fix: rotation in Y and Z are independent now

--- kine-vr/scenes/pivot.gd
+++ kine-vr/scenes/pivot.gd
@@ -4,18 +4,19 @@ extends Node3D
 const ANGULAR_SPEED_THETA : float =  2 * PI 
 const ANGULAR_SPEED_PHI : float = 2 * PI 
 
+var direction : Node3D
+
 func _ready():
-	pass
+	direction = get_child(0)
 
 func _process(delta: float) -> void:
 	
 	# angle movement
 	if Input.is_action_pressed("ui_up"):
-		rotate_z(ANGULAR_SPEED_THETA * delta)
+		direction.rotate_z(ANGULAR_SPEED_THETA * delta)
 	elif Input.is_action_pressed("ui_down"):
-		rotate_z(-ANGULAR_SPEED_THETA * delta)
+		direction.rotate_z(-ANGULAR_SPEED_THETA * delta)
 	elif Input.is_action_pressed("ui_right"):
 		rotate_y(ANGULAR_SPEED_THETA * delta)
 	elif Input.is_action_pressed("ui_left"):
 		rotate_y(-ANGULAR_SPEED_THETA * delta)
-	
