15813bd8439fa73999827c738610a21a88f4375a
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-26 00:47:00 -0300

    feat: add input handling for controller buttons and update button names

--- kine-vr/scripts/controller.gd
+++ kine-vr/scripts/controller.gd
@@ -7,3 +7,43 @@ var buttons_state: Dictionary = {
 	"phi_down": false,
 	"launch": false
 }
+
+
+func _on_theta_up_input_selected() -> void:
+	buttons_state["theta_up"] = true
+
+
+func _on_theta_down_input_selected() -> void:
+	buttons_state["theta_down"] = true
+
+
+func _on_phi_left_input_selected() -> void:
+	buttons_state["phi_left"] = true
+
+
+func _on_phi_right_input_selected() -> void:
+	buttons_state["phi_right"] = true
+
+
+func _on_launch_input_selected() -> void:
+	buttons_state["launch"] = true
+
+
+func _on_theta_up_input_deselected() -> void:
+	buttons_state["theta_up"] = false
+
+
+func _on_theta_down_input_deselected() -> void:
+	buttons_state["theta_down"] = false
+
+
+func _on_phi_left_input_deselected() -> void:
+	buttons_state["phi_left"] = false
+
+
+func _on_phi_right_input_deselected() -> void:
+	buttons_state["phi_right"] = false
+
+
+func _on_launch_input_deselected() -> void:
+	buttons_state["launch"] = false
