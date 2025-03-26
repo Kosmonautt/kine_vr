032d1a419048f1055c5b34973f61249a918e2d5d
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-26 00:10:34 -0300

    feat: simplify input signals in button.gd by removing mode parameter

--- kine-vr/scripts/button.gd
+++ kine-vr/scripts/button.gd
@@ -1,17 +1,16 @@
 extends Node3D
 
-signal input_selected(mode: String)
-signal input_deselected(mode: String)
+signal input_selected()
+signal input_deselected()
 
-@export var mode: String
 
 func _ready() -> void:
 	var button_material: StandardMaterial3D = $pulsor.get_active_material(0)
 
 
 func _on_interactable_area_button_button_pressed(button: Variant) -> void:
-	input_selected.emit(mode)
+	input_selected.emit()
 
 
 func _on_interactable_area_button_button_released(button: Variant) -> void:
-	input_deselected.emit(mode)
+	input_deselected.emit()
