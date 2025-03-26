13a8b7846d041116759611a3d3c916aef1fc020c
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-26 00:11:56 -0300

    feat: add button_color export variable to button.gd for customization

--- kine-vr/scripts/button.gd
+++ kine-vr/scripts/button.gd
@@ -3,6 +3,7 @@ extends Node3D
 signal input_selected()
 signal input_deselected()
 
+@export var button_color: Color
 
 func _ready() -> void:
 	var button_material: StandardMaterial3D = $pulsor.get_active_material(0)
