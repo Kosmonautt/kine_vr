c42e7c381d77df6a0fd1ac1716f5385c0c582566
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-26 01:40:22 -0300

    feat: implement button state handling in pivot script and connect signal for state changes

--- kine-vr/scripts/pivot.gd
+++ kine-vr/scripts/pivot.gd
@@ -10,11 +10,13 @@ const ANGULAR_SPEED_PHI: float = PI
 @export var direction_mesh: Node3D
 @export var projectile: Node3D
 
-var theta_up: bool = false
-var theta_down: bool = false
-var phi_left: bool = false
-var phi_right: bool = false
-var launch: bool = false
+var buttons_state: Dictionary = {
+	"theta_up": false,
+	"theta_down": false,
+	"phi_left": false,
+	"phi_right": false,
+	"launch": false
+}
 
 func _ready() -> void:
 	pass
@@ -23,59 +25,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	# angle movement
-	if Input.is_action_pressed("ui_up") or theta_up:
+	if Input.is_action_pressed("ui_up") or buttons_state["theta_up"]:
 		direction.rotate_z(ANGULAR_SPEED_THETA * delta)
 		projectile.look_at(direction_mesh.global_position, Vector3.UP)
-	elif Input.is_action_pressed("ui_down") or theta_down:
+	elif Input.is_action_pressed("ui_down") or buttons_state["theta_down"]:
 		direction.rotate_z(-ANGULAR_SPEED_THETA * delta)
 		projectile.look_at(direction_mesh.global_position, Vector3.UP)
-	elif Input.is_action_pressed("ui_right") or phi_right:
+	elif Input.is_action_pressed("ui_right") or buttons_state["phi_right"]:
 		rotate_y(-ANGULAR_SPEED_THETA * delta)
 		projectile.look_at(direction_mesh.global_position, Vector3.UP)
-	elif Input.is_action_pressed("ui_left") or phi_left:
+	elif Input.is_action_pressed("ui_left") or buttons_state["phi_left"]:
 		rotate_y(ANGULAR_SPEED_THETA * delta)
 		projectile.look_at(direction_mesh.global_position, Vector3.UP)
 	
-	if Input.is_action_just_pressed("ui_accept") or launch:
+	if Input.is_action_just_pressed("ui_accept") or buttons_state["launch"]:
 		launch_button_pressed.emit(direction_mesh.global_position)
-		launch = false
+		buttons_state["launch"] = false
 
 
-#func _on_theta_up_input_selected(mode: String) -> void:
-	#theta_up = true
-#
-#
-#func _on_theta_down_input_selected(mode: String) -> void:
-	#theta_down = true
-#
-#
-#func _on_phi_left_input_selected(mode: String) -> void:
-	#phi_left = true
-#
-#
-#func _on_phi_r_ight_input_selected(mode: String) -> void:
-	#phi_right = true
-#
-#
-#func _on_launch_input_selected(mode: String) -> void:
-	#launch = true
-#
-#
-#func _on_theta_up_input_deselected(mode: String) -> void:
-	#theta_up = false
-#
-#
-#func _on_theta_down_input_deselected(mode: String) -> void:
-	#theta_down = false
-#
-#
-#func _on_phi_left_input_deselected(mode: String) -> void:
-	#phi_left = false
-#
-#
-#func _on_phi_r_ight_input_deselected(mode: String) -> void:
-	#phi_right = false
-#
-#
-#func _on_launch_input_deselected(mode: String) -> void:
-	#launch = false
+func _on_controller_buttons_state_changed(state: Dictionary) -> void:
+	buttons_state = state
