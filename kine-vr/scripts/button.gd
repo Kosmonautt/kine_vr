f92db0159adeade31eceb86b679452eee14c6c2b
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-26 00:36:49 -0300

    fix: removed color variable for the moment

--- kine-vr/scripts/button.gd
+++ kine-vr/scripts/button.gd
@@ -3,10 +3,10 @@ extends Node3D
 signal input_selected()
 signal input_deselected()
 
-@export var button_color: Color
-
-func _ready() -> void:
-	var button_material: StandardMaterial3D = $pulsor.get_active_material(0)
+#@export var button_color: Color
+#
+#func _ready() -> void:
+	#var button_material: StandardMaterial3D = $pulsor.get_active_material(0)
 
 
 func _on_interactable_area_button_button_pressed(button: Variant) -> void:
