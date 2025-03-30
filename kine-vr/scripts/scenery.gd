cbe5368469be8f6c49faa2b474305b60f3eacf2b
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-30 02:48:38 -0300

    feat: added script to scenery to allow changing the size of the area (WIP)

--- kine-vr/scripts/scenery.gd
+++ kine-vr/scripts/scenery.gd
@@ -1,4 +1,36 @@
 extends Area3D
 
-@export_range(0, 100) var width: int
-@export_range(0, 100) var height: int
+const DEFAULT_WIDTH = 20.0
+const DEFAULT_HEIGHT = 8.0
+const DEFAULT_DEPTH = 6.0
+
+@export var wall: MeshInstance3D
+@export var floor: MeshInstance3D
+@export var collision: CollisionShape3D
+
+@export_range(1.0, 100.0) var width: float = DEFAULT_WIDTH:
+	get:
+		return width
+	set(value):
+		set_size(value, height, depth)
+
+@export_range(1.0, 100.0) var height: float = DEFAULT_HEIGHT:
+	get:
+		return height
+	set(value):
+		set_size(width, value, depth)
+		
+@onready var wall_mesh: PlaneMesh = wall.get_mesh()
+@onready var floor_mesh: PlaneMesh = floor.get_mesh()
+@onready var collision_box_shape: BoxShape3D = collision.get_shape()
+
+@export_range(1.0, 100.0) var depth: float = DEFAULT_DEPTH:
+	get:
+		return depth
+	set(value):
+		set_size(width, height, value)
+
+func set_size(width: float, height: float, depth: float) -> void:
+	wall_mesh.set_size(Vector2(width, height))
+	wall.set_position(Vector3(width/2, 0, 0))
+	
