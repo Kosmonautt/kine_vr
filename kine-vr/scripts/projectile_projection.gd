2009284413fc862a9f1d623a41b9ed8254218a1d
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-03-29 21:27:00 -0300

    fix: update projectile projection modes to use integer values instead of strings

--- kine-vr/scripts/projectile_projection.gd
+++ kine-vr/scripts/projectile_projection.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var projectile: Node3D
-@export var mode: String
+@export_enum("XY", "YZ", "ZX",) var mode: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +12,12 @@ func _ready() -> void:
 func _process(_delta) -> void:
 		
 	# Moves the projection to the projectile position
-	if mode == "XY":
+	if mode == 0:
 		position.x = projectile.position.x
 		position.y = projectile.position.y
-	elif mode == "YZ":
+	elif mode == 1:
 		position.y = projectile.position.y
 		position.z = projectile.position.z
-	elif mode == "ZX":
+	elif mode == 2:
 		position.x = projectile.position.x
 		position.z = projectile.position.z
