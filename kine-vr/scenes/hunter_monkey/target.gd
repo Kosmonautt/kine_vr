4089c5010fd4254e3ae69d24a106106985d0a7bc
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-07 01:53:50 -0400

    feat: added target node and script to it

---
+++ kine-vr/scenes/hunter_monkey/target.gd
@@ -0,0 +1,29 @@
+extends RigidBody3D
+
+const INITIAL_POSITION: Vector3 = Vector3(16.0, 4.0, 3.0)
+
+var can_launch: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = INITIAL_POSITION
+	set_gravity_scale(0.0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	pass
+
+
+func _on_scenery_body_exited(body: Node3D) -> void:
+	if body == self:
+		position = INITIAL_POSITION
+		linear_velocity = Vector3.ZERO
+		set_gravity_scale(0.0)
+		can_launch = true
+
+
+func _on_interactable_area_button_button_pressed(_button: Variant) -> void:
+	if can_launch == true:
+		set_gravity_scale(1.0)
+		can_launch = false
