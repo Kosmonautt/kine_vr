30d1b4342cfa40dd9e298f6360997d9fa77a4ad8
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-07 20:35:05 -0400

    feat: add slider interaction for radius adjustment in pivot script

--- kine-vr/scenes/hunter_monkey/pivot.gd
+++ kine-vr/scenes/hunter_monkey/pivot.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var projectile: Node3D
 @export var target: Node3D
 
-@export var radius: float = 50.0
+var radius: float = 8.0
 
 func _on_interactable_lever_theta_hinge_moved(angle: Variant) -> void:
 	# angle [-45, 45] degrees to [0.0, 1.0]
@@ -32,3 +32,14 @@ func _on_interactable_lever_phi_hinge_moved(angle: Variant) -> void:
 	# set y rotation angle
 	direction.rotation.y = phi
 	projectile.look_at(direction_mesh.global_position, Vector3.UP)
+
+
+func _on_interactable_slider_radius_slider_moved(p: Variant) -> void:
+	# position [0.0, 0.4] meters to [0.0, 1.0]
+	var percentage: float = smoothstep(0.0, 1.0, p)
+	
+	# number [0.0, 1.0] transformed to [8.0, 95.0] meters
+	radius = lerp(8.0, 95.0, percentage)
+	
+	# set target position
+	target.set_position(Vector3(radius * cos(projectile.rotation.x) , radius * sin(projectile.rotation.x) , 3.0))
