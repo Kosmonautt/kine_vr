d316ed8be3019dab8aa2f7843ea7bbedc58d7f17
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-07 21:59:06 -0400

    feat: add rotor script for angular speed and radius control in uniform circular motion scene

---
+++ kine-vr/scenes/uniform_circular_motion/rotor.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var angular_speed: float = PI
+@export var radius: float = 1.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	pass
