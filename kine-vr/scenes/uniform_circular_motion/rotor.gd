d6f3d575499fd17e31b44c5e020cf658e882ca99
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-07 22:04:39 -0400

    feat: implement rotor rotation logic in uniform circular motion scene

--- kine-vr/scenes/uniform_circular_motion/rotor.gd
+++ kine-vr/scenes/uniform_circular_motion/rotor.gd
@@ -4,5 +4,5 @@ extends Node3D
 @export var radius: float = 1.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta) -> void:
-	pass
+func _process(delta) -> void:
+	rotate_z(-angular_speed * delta)
