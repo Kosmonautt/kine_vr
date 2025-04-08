8cb575eff730660a066536dded88f9a92d424c47
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-07 20:53:56 -0400

    feat: add speed slider interaction to control projectile launch speed

--- kine-vr/scenes/hunter_monkey/projectile.gd
+++ kine-vr/scenes/hunter_monkey/projectile.gd
@@ -1,10 +1,10 @@
 extends RigidBody3D
 
 const INITIAL_POSITION: Vector3 = Vector3(0.0, 0.0, 3.0)
-const LAUNCH_SPEED: float = 20
 
 @export var direction_mesh: Node3D
 
+var launch_speed: float = 10
 var can_launch: bool = true
 
 # Called when the node enters the scene tree for the first time.
@@ -30,5 +30,13 @@ func _on_interactable_area_button_button_pressed(_button: Variant) -> void:
 	if can_launch:
 		set_gravity_scale(1.0)
 		var launch_direction: Vector3 = direction_mesh.global_position - global_position
-		linear_velocity = launch_direction.normalized() * LAUNCH_SPEED
+		linear_velocity = launch_direction.normalized() * launch_speed
 		can_launch = false
+
+
+func _on_interactable_slider_speed_slider_moved(p: Variant) -> void:
+	# position [0.0, 0.4] meters to [0.0, 1.0]
+	var percentage: float = smoothstep(0.0, 1.0, p)
+	
+	# number [0.0, 1.0] transformed to [10.0, 100.0] meters/s
+	launch_speed = lerp(10.0, 100.0, percentage)
