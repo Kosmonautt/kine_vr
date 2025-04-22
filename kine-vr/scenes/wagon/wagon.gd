a3996ed737929c708bd725b224c0e3cbd4e2d641
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-21 20:10:37 -0400

    feat: update wagon scene and relative motion setup for improved structure and functionality

--- kine-vr/scenes/wagon/wagon.gd
+++ kine-vr/scenes/wagon/wagon.gd
@@ -3,4 +3,4 @@ extends RigidBody3D
 @export var linear_speed: float = 1.0
 
 func _ready() -> void:
-	set_linear_velocity(Vector3(0.0, 0.0, linear_speed))
+	set_linear_velocity(Vector3(linear_speed, 0.0, 0.0))
