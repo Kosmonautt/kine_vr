f1b7841fa2aa9ba39f75296a6b468bd34d976e76
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-04-21 21:08:35 -0400

    feat: enhance wagon scene with wheel node paths and rotation logic

--- kine-vr/scenes/wagon/wagon.gd
+++ kine-vr/scenes/wagon/wagon.gd
@@ -2,5 +2,19 @@ extends RigidBody3D
 
 @export var linear_speed: float = 1.0
 
+@export var wheel_1: Node3D
+@export var wheel_2: Node3D
+@export var wheel_3: Node3D
+@export var wheel_4: Node3D
+
+var angular_speed: float = PI
+
 func _ready() -> void:
 	set_linear_velocity(Vector3(linear_speed, 0.0, 0.0))
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	wheel_1.rotate_x(angular_speed * delta)
+	wheel_2.rotate_x(angular_speed * delta)
+	wheel_3.rotate_x(angular_speed * delta)
+	wheel_4.rotate_x(angular_speed * delta)
