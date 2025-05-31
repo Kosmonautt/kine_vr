894cfd01fa9a549c9d046e73defea0a4d43d6785
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-05-31 06:14:34 -0400

    feat: changed some momentum variables and kinectic energy of the system is now printed

--- kine-vr/scenes/scenarios/linear_momentum/monetum.gd
+++ kine-vr/scenes/scenarios/linear_momentum/monetum.gd
@@ -4,9 +4,11 @@ extends Node3D
 @export var projectile2: RigidBody3D
 
 func _physics_process(delta: float) -> void:
-	var message_format: String = "p1 momentum: %f  p2 momentum: %f total momentum: %f"
+	var message_format: String = "p1 momentum: %f  p2 momentum: %f total momentum: %f p1 ke: %f"
 	var p1momentum: float = projectile1.get_mass() * projectile1.get_linear_velocity().x
 	var p2momentum: float = projectile2.get_mass() * projectile2.get_linear_velocity().x
-	var message: String = message_format % [p1momentum, p2momentum, p1momentum + p2momentum]
+	var p1ke: float = (projectile1.get_mass() * pow(projectile1.linear_velocity.length(), 2) )/2
+	var p2ke: float = (projectile2.get_mass() * pow(projectile2.linear_velocity.length(), 2) )/2
+	var message: String = message_format % [p1momentum, p2momentum, p1momentum + p2momentum, p1ke + p2ke]
 	
 	print(message)
