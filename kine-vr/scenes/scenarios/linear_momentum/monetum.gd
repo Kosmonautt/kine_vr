a795002843b96ae51e0667eb3c0d10641da6b26a
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-02 21:55:00 -0400

    feat: added potencial gravitational energy and message formatting for debbuging

--- kine-vr/scenes/scenarios/linear_momentum/monetum.gd
+++ kine-vr/scenes/scenarios/linear_momentum/monetum.gd
@@ -4,11 +4,16 @@ extends Node3D
 @export var projectile2: RigidBody3D
 
 func _physics_process(delta: float) -> void:
-	var message_format: String = "p1 momentum: %f  p2 momentum: %f total momentum: %f p1 ke: %f"
+	var message_format: String = "p1 momentum: %f  p2 momentum: %f total momentum: %f p1 Ee: %f"
+	var energy_format: String = "KE: %f, PE: %f, E: %f"
 	var p1momentum: float = projectile1.get_mass() * projectile1.get_linear_velocity().x
 	var p2momentum: float = projectile2.get_mass() * projectile2.get_linear_velocity().x
 	var p1ke: float = (projectile1.get_mass() * pow(projectile1.linear_velocity.length(), 2) )/2
 	var p2ke: float = (projectile2.get_mass() * pow(projectile2.linear_velocity.length(), 2) )/2
-	var message: String = message_format % [p1momentum, p2momentum, p1momentum + p2momentum, p1ke + p2ke]
+	var p1gpe: float = projectile1.get_mass() * 9.8 * (projectile1.get_position().y - 0.5) 
+	var p2gpe: float = projectile2.get_mass() * 9.8 * (projectile1.get_position().y - 0.5)
+	var message: String = message_format % [p1momentum, p2momentum, p1momentum + p2momentum, p1ke + p1gpe]
+	var energy_message: String = energy_format % [p1ke, p1gpe, p1ke + p1gpe]
 	
-	print(message)
+	#print(message)
+	print(energy_message)
