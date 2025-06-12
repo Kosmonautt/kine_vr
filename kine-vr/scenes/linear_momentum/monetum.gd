4756fb8a0f8672737c62f5678e303209cc2eac2a
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-11 22:55:02 -0400

    feat: added player to momentum scene and added code to allow xr use

--- kine-vr/scenes/linear_momentum/monetum.gd
+++ kine-vr/scenes/linear_momentum/monetum.gd
@@ -3,6 +3,23 @@ extends Node3D
 @export var projectile1: RigidBody3D
 @export var projectile2: RigidBody3D
 
+var xr_interface: XRInterface
+
+func _ready() -> void:
+	Engine.max_fps = 90
+	
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
+
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		# Change our main viewport to output to the HMD
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
+
 func _physics_process(delta: float) -> void:
 	var message_format: String = "p1 momentum: %f  p2 momentum: %f total momentum: %f p1 Ee: %f"
 	var energy_format: String = "KE: %f, PE: %f, E: %f"
