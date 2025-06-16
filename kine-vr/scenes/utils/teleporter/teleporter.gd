126ee37143f698ef1ab5813651b5b9df0a0acc5b
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-16 19:05:28 -0400

    feat: teleporter now uses packed scenes

--- kine-vr/scenes/utils/teleporter/teleporter.gd
+++ kine-vr/scenes/utils/teleporter/teleporter.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var packed_scene: PackedScene
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player_body"):
-		get_tree().change_scene_to_file("res://scenes/parabola/parabola.tscn")
+		get_tree().change_scene_to_packed(packed_scene)
