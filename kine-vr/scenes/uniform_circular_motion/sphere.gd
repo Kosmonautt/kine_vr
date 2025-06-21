4170744be7314655ec28febd1fe5b520be06a2a5
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-20 21:38:06 -0400

    feat: decal is spawned in sine plane

--- kine-vr/scenes/uniform_circular_motion/sphere.gd
+++ kine-vr/scenes/uniform_circular_motion/sphere.gd
@@ -3,12 +3,20 @@ extends MeshInstance3D
 @export var rotor_sphere: MeshInstance3D
 @export var plane: Node3D
 @export_enum("Sine", "Cosine") var mode: int
+@export var decal_scene: PackedScene
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# sine mode
 	if mode == 0:
+		# sphere follows rotor
 		set_global_position(Vector3(4, rotor_sphere.get_global_position().y, -4))
+		# a decal is spawned
+		var decal = decal_scene.instantiate()
+		# decal is added to the plane
+		plane.add_child(decal)
+		# the initial position is set
+		decal.set_global_position(Vector3(get_global_position()))
 	# cosine mode
 	if mode == 1:
 		set_global_position(Vector3(rotor_sphere.get_global_position().x, 0, -4))
