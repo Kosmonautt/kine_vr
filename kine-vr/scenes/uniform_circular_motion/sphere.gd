8ffeaa38ead99a5359f7fe81a8e4eb1ef8db8091
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-23 19:10:54 -0400

    feat: added code to change mesh color of decals in UCM scene

--- kine-vr/scenes/uniform_circular_motion/sphere.gd
+++ kine-vr/scenes/uniform_circular_motion/sphere.gd
@@ -5,6 +5,10 @@ extends MeshInstance3D
 @export_enum("Sine", "Cosine") var mode: int
 @export var decal_scene: PackedScene
 
+# colors for decals
+const sine_decal_color = Color(0, 1, 0)
+const cosine_decal_color = Color(0, 0, 1)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# sine mode
@@ -23,3 +27,10 @@ func _on_timer_timeout() -> void:
 	plane.add_child(decal)
 	# the initial position is set
 	decal.set_global_position(Vector3(get_global_position()))
+	# color change
+	# sine mode
+	if mode == 0:
+		decal.change_color(sine_decal_color)
+	# cosine mode
+	elif mode == 1:
+		decal.change_color(cosine_decal_color)
