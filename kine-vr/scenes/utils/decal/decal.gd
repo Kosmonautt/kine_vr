ca6c092bb4ec6d196c0d2400d57bc3a665290b29
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-22 22:21:23 -0400

    feat: added methos to change decal color with Color object

--- kine-vr/scenes/utils/decal/decal.gd
+++ kine-vr/scenes/utils/decal/decal.gd
@@ -6,3 +6,7 @@ extends Node3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	set_global_position(Vector3(get_global_position().x, get_global_position().y, get_global_position().z + (speed * delta)))
+
+func change_color(color: Color) -> void:
+	# change mesh color
+	$MeshInstance3D.get_mesh().surface_get_material(0).albedo_color = color
