a98376c0fb1f60fb0f5779422dc3b35c223e89d5
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-23 22:30:40 -0400

    feat: added code to target script to show decals

--- kine-vr/scenes/hunter_monkey/target.gd
+++ kine-vr/scenes/hunter_monkey/target.gd
@@ -28,13 +28,35 @@ func _on_scenery_body_exited(body: Node3D) -> void:
 		linear_velocity = Vector3.ZERO
 		set_gravity_scale(0.0)
 		can_launch = true
+		# decals stop spawning
+		timer.stop()
 
 
 func _on_interactable_area_button_button_pressed(_button: Variant) -> void:
 	if can_launch == true:
 		set_gravity_scale(1.0)
 		can_launch = false
+		# if decal list is not empty
+		if not decal_list.is_empty():
+			# all decals are deleted
+			for d in decal_list:
+				d.queue_free()
+			# list is emptied
+			decal_list.clear()
+		# decals start spawning
+		timer.start()
 
 
 func _on_target_timer_timeout() -> void:
-	pass # Replace with function body.
+	# decals are spawned
+	var decal_xy = decal_scene.instantiate()
+	# decal speed is set to 0
+	decal_xy.speed = 0
+	# decal are added to the plane
+	scenery.add_child(decal_xy)
+	# the initial positions are set
+	decal_xy.set_global_position(Vector3(get_global_position().x, get_global_position().y, scenery.get_global_position().z))
+	# add decal references to list
+	decal_list.append(decal_xy)
+	# color change
+	decal_xy.change_color(decal_color)
