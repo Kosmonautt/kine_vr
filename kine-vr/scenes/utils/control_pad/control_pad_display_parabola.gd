835f76d24120cb665811e5d0d4b7f566042c91d9
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-25 22:10:01 -0400

    fix: using correct script file now

--- kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
+++ kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
@@ -1,1 +1,10 @@
 extends TabContainer
+
+var num: float = 0.0
+
+func _ready() -> void:
+	$Data/VBoxContainer/ThetaAngle/Value.text = "%8.3f" % [num]
+
+func _on_refresh_timer_timeout() -> void:
+	num += 0.1
+	$Data/VBoxContainer/ThetaAngle/Value.text = "%8.3f" % [num]
