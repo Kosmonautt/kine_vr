7ec1a7ab52abe9ac8c79826130bb2a27d4c9e3ce
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-26 16:41:44 -0400

    feat: using get functions instead of parameter for position, velocity and rotation, also position is now local, not global

--- kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
+++ kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
@@ -3,9 +3,9 @@ extends TabContainer
 var num: float = 0.0
 
 func refresh_screen(projectile: RigidBody3D) -> void:
-	var pos: Vector3 = projectile.global_position
-	var vel: Vector3 = projectile.linear_velocity
-	var rot: Vector3 = projectile.rotation_degrees
+	var pos: Vector3 = projectile.get_position()
+	var vel: Vector3 = projectile.get_linear_velocity()
+	var rot: Vector3 = projectile.get_rotation_degrees()
 	var pos_str := "%8.3f, %8.3f, %8.3f" % [pos.x, pos.y, pos.z]
 	var vel_str := "%8.3f, %8.3f, %8.3f" % [vel.x, vel.y, vel.z]
 	var rot_str := "%8.3f, %8.3f, %8.3f" % [rot.x, rot.y, rot.z]
