63f8d3680dfbfabf321fd3ab185119e62e18d263
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-25 23:35:50 -0400

    feat fix: changed type for refresh_screen and all variables are shown now

--- kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
+++ kine-vr/scenes/utils/control_pad/control_pad_display_parabola.gd
@@ -2,6 +2,15 @@ extends TabContainer
 
 var num: float = 0.0
 
-func refresh_screen(angle: StaticBody3D) -> void:
-	$Data/VBoxContainer/ThetaAngle/Value.text = "Test"
+func refresh_screen(projectile: RigidBody3D) -> void:
+	var pos: Vector3 = projectile.global_position
+	var vel: Vector3 = projectile.linear_velocity
+	var rot: Vector3 = projectile.rotation_degrees
+	var pos_str := "%8.3f, %8.3f, %8.3f" % [pos.x, pos.y, pos.z]
+	var vel_str := "%8.3f, %8.3f, %8.3f" % [vel.x, vel.y, vel.z]
+	var rot_str := "%8.3f, %8.3f, %8.3f" % [rot.x, rot.y, rot.z]
+
+	$Data/VBoxContainer/Position/Value.text = pos_str
+	$Data/VBoxContainer/Velocity/Value.text = vel_str
+	$Data/VBoxContainer/ThetaAngle/Value.text = rot_str
 	
