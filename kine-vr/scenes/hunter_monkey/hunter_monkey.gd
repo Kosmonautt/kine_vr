67b31bf01f48cda869cf23fd8f33c85afd1df600
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-06-30 21:04:56 -0400

    feat: added display for projectile and target in hunter_monkey scene

--- kine-vr/scenes/hunter_monkey/hunter_monkey.gd
+++ kine-vr/scenes/hunter_monkey/hunter_monkey.gd
@@ -2,6 +2,14 @@ extends Node3D
 
 var xr_interface: XRInterface
 
+var projectile_control: TabContainer
+var target_control: TabContainer
+
+@export var projectile: RigidBody3D
+@export var target: RigidBody3D
+@export var projectile_refresh_timer: Timer
+@export var target_refresh_timer: Timer
+
 func _ready() -> void:
 	Engine.max_fps = 90
 	
@@ -16,3 +24,19 @@ func _ready() -> void:
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+	
+	projectile_control = $ControlPadProjectile/Viewport2dIn3d.get_scene_instance()
+	projectile_control.set_label_name("Projectile")
+	
+	target_control = $ControlPadTarget/Viewport2dIn3d.get_scene_instance()
+	target_control.set_label_name("Target")
+	
+	
+func _on_projectile_refresh_timer_timeout() -> void:
+	projectile_control.refresh_screen(projectile)
+	projectile_refresh_timer.start()
+
+
+func _on_target_refresh_timer_timeout() -> void:
+	target_control.refresh_screen(target)
+	target_refresh_timer.start()
