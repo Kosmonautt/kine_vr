f65eddc857a688f67f3df4304a79956b566d3897
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-07-05 21:33:09 -0400

    feat: added controls labels and monitoring to control pad display scene

--- kine-vr/scenes/utils/control_pad/control_pad_display_hunter_monkey.gd
+++ kine-vr/scenes/utils/control_pad/control_pad_display_hunter_monkey.gd
@@ -8,9 +8,9 @@ func refresh_screen(projectile: RigidBody3D, target: RigidBody3D) -> void:
 	var proj_vel_str := "%8.3f, %8.3f, %8.3f" % [proj_vel.x, proj_vel.y, proj_vel.z]
 	var proj_rot_str := "%8.3f, %8.3f, %8.3f" % [proj_rot.x, proj_rot.y, proj_rot.z]
 
-	$Data/HBoxContainer/VBoxContainer/Position/Value.text = proj_pos_str
-	$Data/HBoxContainer/VBoxContainer/Velocity/Value.text = proj_vel_str
-	$Data/HBoxContainer/VBoxContainer/Angle/Value.text = proj_rot_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer/Position/Value.text = proj_pos_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer/Velocity/Value.text = proj_vel_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer/Angle/Value.text = proj_rot_str
 
 	var tar_pos: Vector3 = target.get_position()
 	var tar_vel: Vector3 = target.get_linear_velocity()
@@ -19,13 +19,13 @@ func refresh_screen(projectile: RigidBody3D, target: RigidBody3D) -> void:
 	var tar_vel_str := "%8.3f, %8.3f, %8.3f" % [tar_vel.x, tar_vel.y, tar_vel.z]
 	var tar_rot_str := "%8.3f, %8.3f, %8.3f" % [tar_rot.x, tar_rot.y, tar_rot.z]
 
-	$Data/HBoxContainer/VBoxContainer2/Position/Value.text = tar_pos_str
-	$Data/HBoxContainer/VBoxContainer2/Velocity/Value.text = tar_vel_str
-	$Data/HBoxContainer/VBoxContainer2/Angle/Value.text = tar_rot_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer2/Position/Value.text = tar_pos_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer2/Velocity/Value.text = tar_vel_str
+	$Data/HBoxContainer/VBoxContainer/VBoxContainer2/Angle/Value.text = tar_rot_str
 
 func set_label_name(name: String, pos: int) -> void:
 	if pos == 0:
-		$Data/HBoxContainer/VBoxContainer/Name.text = name
+		$Data/HBoxContainer/VBoxContainer/VBoxContainer/Name.text = name
 	elif pos == 1:
-		$Data/HBoxContainer/VBoxContainer2/Name.text = name
+		$Data/HBoxContainer/VBoxContainer/VBoxContainer2/Name.text = name
 	
